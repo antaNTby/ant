b9c8c6d2185d263263badb76d2318d44f69a7139
Alexander <nixminsk@gmail.com>
2025-11-06 11:40:41 +0300

    ddddaqsdasdasd

--- ant.my/index.php
+++ ant.my/index.php
@@ -74,31 +74,6 @@ function jlog(
     return true;
 }
 
-/*
-
-// If you're using Composer, require the autoloader.
-require 'vendor/autoload.php';
-// if you're not using Composer, load the framework directly
-// require 'flight/Flight.php';
-
-// Then define a route and assign a function to handle the request.
-Flight::route( '/', function () {
-    echo 'hello world!';
-} );
-// Then define a route and assign a function to handle the request.
-Flight::route( '/admin', function () {
-    echo 'hello world admin!';
-} );
-
-Flight::route( '/page', function () {
-    echo 'hello site!';
-} );
-
-// Finally, start the framework.
-Flight::start();
-
-*/
-
 define( '__PARENT_DIR__', dirname( __DIR__, 1 ) );
 define( '__ROOT__', __DIR__ );
 define( '__PUBLIC__', __ROOT__ . DIRECTORY_SEPARATOR . 'public' );
