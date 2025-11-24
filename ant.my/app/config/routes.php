4834608c5b2c6037089cdc5219ea360b584d6b29
miniITX <nixminsk@gmail.com>
2025-11-24 12:36:12 +0300

    Flight::route( 'GET /', function () {
        $contentTemplate = __TPL__ . DIRECTORY_SEPARATOR . 'home.tpl.html';
        $smartyData      = [];
        Flight::fetch( $contentTemplate, $smartyData );
        Flight::render( 'index.tpl.html', [
            'admin_main_content_template' => $contentTemplate,
            'body_height'                 => Flight::get( 'body_height' ),
            'body_width'                  => Flight::get( 'body_width' ),
            'template_exist'              => Flight::view()->templateExists( $contentTemplate ),
            'pageH1'                      => 'Стартовая страница ',
            'title'                       => 'Стартовая страница ',
            'title_ru'                    => 'Просмотр Стартовая страница',
            'aside_reverse'               => Flight::get( 'aside_reverse' ),
            'aside_hide'                  => Flight::get( 'aside_hide' ),
        ] );
    } );

--- ant.my/app/config/routes.php
+++ ant.my/app/config/routes.php
@@ -1,1 +1,31 @@
 <?php
+
+Flight::route( 'GET /', function () {
+
+    $contentTemplate = __TPL__ . DIRECTORY_SEPARATOR . 'home.tpl.html';
+    $smartyData      = [];
+    Flight::fetch( $contentTemplate, $smartyData );
+    Flight::render( 'index.tpl.html', [
+        'admin_main_content_template' => $contentTemplate,
+        'body_height'                 => Flight::get( 'body_height' ),
+        'body_width'                  => Flight::get( 'body_width' ),
+        'template_exist'              => Flight::view()->templateExists( $contentTemplate ),
+        'pageH1'                      => 'Стартовая страница ',
+        'title'                       => 'Стартовая страница ',
+        'title_ru'                    => 'Просмотр Стартовая страница',
+        'aside_reverse'               => Flight::get( 'aside_reverse' ),
+        'aside_hide'                  => Flight::get( 'aside_hide' ),
+    ] );
+} );
+
+Flight::route( 'OPTIONS *', function () {
+    // https: //docs.flightphp.com/learn/security#cors
+    header( 'Access-Control-Allow-Origin: *' );
+    // header('Access-Control-Allow-Origin: https://your-domain.com');
+    header( 'Access-Control-Allow-Methods: GET, POST, OPTIONS' );
+    header( 'Access-Control-Allow-Headers: Content-Type, Authorization' );
+    header( 'Access-Control-Max-Age: 86400' ); // Кеширование
+    Flight::halt( 200 );
+} );
+
+// $app->render( 'index.tpl.html', ['app' => $app] );
