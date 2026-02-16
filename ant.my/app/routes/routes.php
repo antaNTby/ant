b42787765b0f49a56781465659dfbc38824ab97f
antEXPERT <antaNT.pro@gmail.com>
2026-02-16 13:25:52 +0300

    Tracking Prevention blocked access to storage for https://cdn.jsdelivr.net/.

--- ant.my/app/routes/routes.php
+++ ant.my/app/routes/routes.php
@@ -10,12 +10,6 @@ use flight\util\Json;
 $router = $app->router();
 /**/
 
-#### Если вы хотите применить глобальный middleware ко всем вашим маршрутам, вы можете добавить "пустую" группу:
-// добавлено в конце метода группы
-Flight::group( '/admin', function ( Router $router ) {
-
-}, [LoggedInMiddleware::class] ); // или [ new ApiAuthMiddleware() ], одно и то же
-
 Flight::route( '*', function () {
     $session = Flight::session();
 
@@ -34,22 +28,4 @@ Flight::route( '*', function () {
         $rednderData
     );
 
-} );
-
-// Страница логина
-Flight::route( 'GET /login', function () {
-    $session = Flight::session();
-    Flight::render( 'layout.tpl.html', [
-        'title'        => 'Авторизация',
-        'info_message' => 'error', // Получаем flash-сообщение
-    ] );
-} );
-
-// Страница логаута (если нужен шаблон)
-Flight::route( 'GET /logout', function () {
-    $session = Flight::session();
-    $session->clear();
-    Flight::render( 'layout.tpl.html', [
-        'title' => 'Выход из системы',
-    ] );
 } );
---
+++ ant.my/trash/routes/routes.php
@@ -0,0 +1,55 @@
+<?php
+/*use app\controllers\ApiExampleController;*/
+/*use app\middlewares\SecurityHeadersMiddleware;*/
+use flight\Engine;
+use flight\net\Router;
+use flight\Session;
+use flight\util\Json;
+
+// Whip out the ol' router and we'll pass that to the routes file
+$router = $app->router();
+/**/
+
+#### Если вы хотите применить глобальный middleware ко всем вашим маршрутам, вы можете добавить "пустую" группу:
+// добавлено в конце метода группы
+Flight::group( '/admin', function ( Router $router ) {
+
+}, [LoggedInMiddleware::class] ); // или [ new ApiAuthMiddleware() ], одно и то же
+
+Flight::route( '*', function () {
+    $session = Flight::session();
+
+    // dump( Flight::router() );
+    $rednderData = [
+
+        'app'       => Flight::app(),
+        'year'      => date( 'Y' ),
+        'title'     => SERVER_NAME . ' ' . date( 'Y' ) . '-' . date( 'M' ) . '-' . date( 'd' ) . ' ' . date( 'H' ) . ':' . date( 'm' ) . ':' . date( 'i' ),
+        'COPYRIGHT' => COPYRIGHT,
+        // 'BRANDNAME' => BRANDNAME,
+
+    ];
+
+    Flight::render( 'home/index.tpl.html',
+        $rednderData
+    );
+
+} );
+
+// Страница логина
+Flight::route( 'GET /login', function () {
+    $session = Flight::session();
+    Flight::render( 'layout.tpl.html', [
+        'title'        => 'Авторизация',
+        'info_message' => 'error', // Получаем flash-сообщение
+    ] );
+} );
+
+// Страница логаута (если нужен шаблон)
+Flight::route( 'GET /logout', function () {
+    $session = Flight::session();
+    $session->clear();
+    Flight::render( 'layout.tpl.html', [
+        'title' => 'Выход из системы',
+    ] );
+} );
