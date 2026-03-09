957a8a6096b5c0823dfcca8f708c5fd218fe6e8f
antEXPERT <antaNT.pro@gmail.com>
2026-03-09 12:25:45 +0300

    C:\git\ant\ant.my\vendor\bin>runway make:controller BaseSub Controller successfully created at C:\git\ant\ant.my/app/controllers/BaseSubController.php

---
+++ ant.my/app/controllers/BaseSubController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers;
+
+use flight\Engine;
+
+class BaseSubController
+{
+    /** @var Engine */
+    protected Engine $app;
+
+    /**
+     * Constructor
+     */
+    public function __construct(Engine $app)
+    {
+        $this->app = $app;
+    }
+}
